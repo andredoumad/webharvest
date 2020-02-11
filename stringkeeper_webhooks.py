c5c3185e8d0fca2dccf5556ac9f5013316421410
andre <andre@stringkeeper.com>
2020-02-11 00:55:12 -0800

    tr3b

--- stringkeeper_webhooks.py
+++ stringkeeper_webhooks.py
@@ -1,6 +1,10 @@
 import json
 import requests
 from datetime import datetime
+import asyncio
+import logging
+import websockets
+logging.basicConfig(level=logging.INFO)
 
 class WebHarvestWorker:
 
@@ -35,6 +39,7 @@ class WebHarvestWorker:
         })
 
 
+
 worker = WebHarvestWorker('Alice')
 print(worker.name)
 
@@ -44,3 +49,21 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 worker.chat_message(str('My name is ' + str(worker.name) + '. The time is: ' + dt_string + '.'))
 print('response: ' + str(worker.response))
 print('response json: ' + str(worker.response.json()))
+
+
+
+# async def command_receiver():
+#     async with websockets.connect('ws://127.0.0.1:80/webharvest/') as websocket:
+#         while True:
+#             message = await websocket.recv()
+#             await websocket.send("Received the command '{message}'")
+#             if message == "start":
+#                 print('received message start')
+#             elif message == "stop":
+#                 print('received message stop')
+#             else:
+#                 await websocket.send("Unknown command")     
+
+
+# # asyncio.get_event_loop().run_until_complete(command_receiver())
+# asyncio.get_event_loop().run_until_complete(command_receiver())
